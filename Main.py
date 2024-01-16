dc58ce6582390e1c711d3b2624859958fb13d276
Felipe Guimaraens <47477178+felipeguimaraens@users.noreply.github.com>
2024-01-16 18:41:56 -0300

    Uploading Main.py

---
+++ Main.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+from re import sub
+import requests as r
+import pandas as pd
+
+URL = 'https://requests.readthedocs.io/en/latest/api/'
+
+word_list = []
+new_list = []
+
+if 'http' not in URL:
+    URL = 'https://' + URL
+response = r.get(URL)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+content = soup.text
+
+content = content.split(' ')
+for word in content:
+    if word != '' and len(word) >= 2:
+        word = sub('[^A-Za-z0-9]+', '', word)
+        new_list.append(word)
+
+data = pd.DataFrame(new_list)
+data = data.value_counts()
+print(data)
+
+
+#content = re.sub('[^A-Za-z0-9]+', '', content)
+
+#print(word_list)
+
+# text = response.text
+# SAVE FILE
+#with open('page.html', 'w', encoding='utf-8') as file:
+#   file.write(text)
